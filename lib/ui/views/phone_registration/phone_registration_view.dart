d793c8bcde7454c6c0ccdf94adb0926b8023b7f1
suheerthedev <khansuheer1@gmail.com>
2024-12-03 22:32:45 +0500

    Updated phone registration view and model

--- lib/ui/views/phone_registration/phone_registration_view.dart
+++ lib/ui/views/phone_registration/phone_registration_view.dart
@@ -1,28 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:stacked/stacked.dart';
-
 import 'phone_registration_viewmodel.dart';
 
-class PhoneRegistrationView extends StackedView<PhoneRegistrationViewModel> {
-  const PhoneRegistrationView({Key? key}) : super(key: key);
+class PhoneRegistrationView extends StatelessWidget {
+  const PhoneRegistrationView({super.key});
 
   @override
-  Widget builder(
-    BuildContext context,
-    PhoneRegistrationViewModel viewModel,
-    Widget? child,
-  ) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Container(
-        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<PhoneRegistrationViewModel>.reactive(
+      viewModelBuilder: () => PhoneRegistrationViewModel(),
+      builder: (context, model, child) => Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              const Text(
+                'ENTER YOU NUMBER',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+                textAlign: TextAlign.center,
+              ),
+              Column(
+                children: [
+                  TextField(
+                    controller: model.phoneCont,
+                    decoration: InputDecoration(
+                      hintText: '+92-xxx-xxxxxxx',
+                      labelText: 'Phone Number',
+                      labelStyle: const TextStyle(color: Colors.black),
+                      suffixIcon: const Icon(
+                        FontAwesomeIcons.phone,
+                        color: Color.fromARGB(255, 133, 203, 135),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.black),
+                          borderRadius: BorderRadius.circular(10)),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.green),
+                          borderRadius: BorderRadius.circular(10)),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                    keyboardType: TextInputType.phone,
+                  ),
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        backgroundColor: Colors.green,
+                      ),
+                      onPressed: () {
+                        model.submitPhoneNumber(model.phoneCont.text.trim());
+                      },
+                      child: const Text(
+                        'Next',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 300,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
-
-  @override
-  PhoneRegistrationViewModel viewModelBuilder(
-    BuildContext context,
-  ) =>
-      PhoneRegistrationViewModel();
 }
--- lib/ui/views/phone_registration/phone_registration_viewmodel.dart
+++ lib/ui/views/phone_registration/phone_registration_viewmodel.dart
@@ -1,3 +1,33 @@
+import 'package:alwan_chat_app/app/app.locator.dart';
+import 'package:alwan_chat_app/app/app.router.dart';
+import 'package:alwan_chat_app/services/phone_auth_service.dart';
+import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 
-class PhoneRegistrationViewModel extends BaseViewModel {}
+class PhoneRegistrationViewModel extends BaseViewModel {
+  final NavigationService navigationService = NavigationService();
+  final PhoneAuthService _phoneAuthService = locator<PhoneAuthService>();
+  final TextEditingController phoneCont = TextEditingController();
+  final DialogService _dialogService = locator<DialogService>();
+
+  Future<void> submitPhoneNumber(String phoneNumber) async {
+    setBusy(true);
+    try {
+      await _phoneAuthService.sendOtp(phoneNumber);
+      navigationService.navigateToOtpVerificationView();
+    } catch (e) {
+      _dialogService.showDialog(
+        title: 'Error',
+        description: e.toString(),
+      );
+    }
+    setBusy(false);
+  }
+
+  @override
+  void dispose() {
+    phoneCont.dispose();
+    super.dispose();
+  }
+}
