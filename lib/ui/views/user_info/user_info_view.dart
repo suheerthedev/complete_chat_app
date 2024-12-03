c6206129d0228c33b5954dd0544b493fa7fdae5a
suheerthedev <khansuheer1@gmail.com>
2024-12-03 22:34:04 +0500

    Updated user info view

--- lib/ui/views/user_info/user_info_view.dart
+++ lib/ui/views/user_info/user_info_view.dart
@@ -1,28 +1,135 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
-
 import 'user_info_viewmodel.dart';
 
-class UserInfoView extends StackedView<UserInfoViewModel> {
-  const UserInfoView({Key? key}) : super(key: key);
+class UserInfoView extends StatelessWidget {
+  const UserInfoView({super.key});
 
   @override
-  Widget builder(
-    BuildContext context,
-    UserInfoViewModel viewModel,
-    Widget? child,
-  ) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Container(
-        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+  Widget build(BuildContext context) {
+    final List<String> genderOptions = ['Male', 'Female', 'Pakistani', 'Other'];
+
+    return ViewModelBuilder<UserInfoViewModel>.reactive(
+      viewModelBuilder: () => UserInfoViewModel(),
+      builder: (context, model, child) => Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              //Title and Subtitle Here
+              const Text(
+                'Complete Your Profile',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+                textAlign: TextAlign.center,
+              ),
+
+              // Profile Avatar here
+              GestureDetector(
+                onTap: model.pickImage,
+                child: CircleAvatar(
+                  backgroundColor: Colors.green,
+                  radius: 60,
+                  backgroundImage: model.profileImage != null
+                      ? FileImage(model.profileImage!) // Show picked image
+                      : null,
+                  child: model.profileImage == null
+                      ? const Icon(Icons.add_a_photo,
+                          size: 40, color: Colors.white)
+                      : null,
+                ),
+              ),
+
+              Column(
+                children: [
+                  TextField(
+                    controller: model.nameCont,
+                    decoration: InputDecoration(
+                      labelText: 'Enter your name',
+                      labelStyle: const TextStyle(color: Colors.black),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.black),
+                          borderRadius: BorderRadius.circular(10)),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.green),
+                          borderRadius: BorderRadius.circular(10)),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                    keyboardType: TextInputType.name,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  TextField(
+                    controller: model.bioCont,
+                    decoration: InputDecoration(
+                      labelText: 'Enter your bio',
+                      labelStyle: const TextStyle(color: Colors.black),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.black),
+                          borderRadius: BorderRadius.circular(10)),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.green),
+                          borderRadius: BorderRadius.circular(10)),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                    keyboardType: TextInputType.name,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  DropdownButtonFormField(
+                    borderRadius: BorderRadius.circular(10),
+                    decoration: InputDecoration(
+                      labelText: 'Select Gender',
+                      labelStyle: const TextStyle(color: Colors.black),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.black),
+                          borderRadius: BorderRadius.circular(10)),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.green),
+                          borderRadius: BorderRadius.circular(10)),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                    // value: selectedGender,
+                    items: genderOptions.map((String gender) {
+                      return DropdownMenuItem<String>(
+                        value: gender,
+                        child: Text(gender),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {},
+                  )
+                ],
+              ),
+
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    backgroundColor: Colors.green,
+                  ),
+                  onPressed: model.submitUserInfo,
+                  child: const Text(
+                    'Done',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
-
-  @override
-  UserInfoViewModel viewModelBuilder(
-    BuildContext context,
-  ) =>
-      UserInfoViewModel();
 }
--- lib/ui/views/user_info/user_info_viewmodel.dart
+++ lib/ui/views/user_info/user_info_viewmodel.dart
@@ -1,3 +1,33 @@
-import 'package:stacked/stacked.dart';
+import 'dart:io';
 
-class UserInfoViewModel extends BaseViewModel {}
+import 'package:alwan_chat_app/app/app.router.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+class UserInfoViewModel extends BaseViewModel {
+  final NavigationService navigationService = NavigationService();
+  final TextEditingController nameCont = TextEditingController();
+  final TextEditingController bioCont = TextEditingController();
+
+  File? _profileImage;
+
+  File? get profileImage => _profileImage;
+
+  Future<void> pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(
+      source: ImageSource.gallery, // Use ImageSource.camera for camera
+    );
+
+    if (pickedFile != null) {
+      _profileImage = File(pickedFile.path);
+      notifyListeners(); // Notify the UI to rebuild
+    }
+  }
+
+  void submitUserInfo() {
+    navigationService.replaceWithHomeView();
+  }
+}
